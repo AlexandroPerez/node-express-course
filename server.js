b25ce90e7d7e31409e2b28e9ae298363f3e04994
Alexandro Perez <apgfriend@gmail.com>
2021-06-09 03:26:14 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+// In Express >= 4.16.0 body parser re-added under express.json() and express.urlencoded()
+const bodyParser = require("body-parser");
+app.use(express.json());
+
 const mockUserData = [{ name: "Mark" }, { name: "jill" }];
 app.get("/users", (req, res) => {
   res.json({
@@ -17,6 +21,27 @@ app.get("/users/:id", (req, res) => {
     message: "got one user",
     user: req.params.id,
   });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match our records!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and/or username do not match our records",
+    });
+  }
 });
 
 app.listen(8000, () => {
